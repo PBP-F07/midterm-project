1733e36188fea30d117771754a4cda78b9fc8686
Vincent Suhardi <vinjupt@gmail.com>
2023-10-23 10:12:34 +0700

    menambahkan path logout

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from authentication.views import register, login_user
+from authentication.views import register, login_user, logout_user
 
 app_name = 'authentication'
 
 urlpatterns = [
     path('', login_user, name='login'),
-    path('register/', register, name='register')
+    path('register/', register, name='register'),
+    path('logout/', logout_user, name='logout')
 ]
