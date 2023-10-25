19b02215191dc03cbfb4ba3c280462ac44a8ad99
dienzahraaa <dienzahra312@gmail.com>
2023-10-26 01:21:28 +0700

    mengerjakan fungsionalitas wishlist

--- wishlist_page/urls.py
+++ wishlist_page/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from wishlist_page.views import show_main
+from wishlist_page.views import search_books, add_to_wishlist, load_wishlist
 
 app_name = 'wishlist_page'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path(' ', search_books, name='search_books'),
+    path('add_to_wishlist/', add_to_wishlist, name='add_to_wishlist'),
+    path('load_wishlist/', load_wishlist, name='load_wishlist'),
+    
 ]
