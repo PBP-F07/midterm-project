a07671404944ef284a21f98b9d440bfee40f0bea
Vincent Suhardi <vinjupt@gmail.com>
2023-10-26 17:55:47 +0700

    modify show_main func

--- admin_page/views.py
+++ admin_page/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
+from wishlist_page import models
 
 @login_required(login_url='')
 def show_main(request):
     user_role = request.user.groups.all()[0].name
-    print(user_role + "login")
 
-    return HttpResponse("Ini halaman admin literaphile")
+    context = {
+        'name': request.user.username
+    }
+
+    return render(request, "admin.html", context)
+
