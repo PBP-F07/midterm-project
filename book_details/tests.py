744b36100c3ecddf3ca69213e8a91d3ce3c1835b
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-10-29 18:54:09 +0700

    finishing book_detail

--- book_details/tests.py
+++ book_details/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from .models import discussion, reply
+from landingPage.models import Books
 
-# Create your tests here.
+
+class mainTest(TestCase):
+
+    def setUp_discussion(self):
+        book1 = Books.objects.create(
+            title="pbp",
+            author="pbp",
+            description="pbp",
+            image="http://books.google.com/books/content?id=aJQILlLxRmAC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+            year_of_release=2005,
+            amount=5
+        )
+        self.item = discussion.objects.create(
+            book = book1,
+            comment = "halo semua",
+            user = "halo",
+        )
+        
+    def test_discussion_creation(self):
+        self.assertEqual(self.item.comment, "halo semua")
+        self.assertEqual(self.item.user,"halo")
+
