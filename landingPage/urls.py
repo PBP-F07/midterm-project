7c66cebc35506b5b5ef954dd9d37260ebd7f44fe
Vincent Suhardi <vinjupt@gmail.com>
2023-10-29 15:42:34 +0700

    resolve admin_page problem

--- landingPage/urls.py
+++ landingPage/urls.py
@@ -5,11 +5,9 @@ from wishlist_page.views import search_books
 app_name = 'landingPage'
 
 urlpatterns = [
-    path(' ', show_main, name='show_main'),
+    path('', show_main, name='show_main'),
     path('get_books/', get_books_json, name='get_books_json'),
     path('search_books_static/', search_books_static, name='search_books_static'),
     path('borrow_book/<int:id>/', borrow_book, name='borrow_book'),
-    path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
+    path('json/<int:id>/', show_json_by_id, name='show_json_by_id')
 ]
-
-fetch_static_books()
