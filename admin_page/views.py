6f50b8ddf3c0a92d7c2f232940631952ae492731
dienzahraaa <dienzahra312@gmail.com>
2023-10-29 20:29:50 +0700

    debugging

--- admin_page/views.py
+++ admin_page/views.py
@@ -52,9 +52,12 @@ def show_managebooks(request):
     return render(request, "managebooks.html", context)
 
 def show_notes(request):
-    notes = newWishlist.objects.filter(user=request.user)
+    notes = newWishlist.objects.all()
+    groups = request.user.groups.all()
 
     context = {
+        'name': request.user.username,
+        'role': groups[0].name,
         'notes': notes
     }
     return render(request, "notes.html", context)
