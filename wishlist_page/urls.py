203a0e179b4dfcc0a90438dee963f60dbab76367
dienzahraaa <dienzahra312@gmail.com>
2023-10-29 14:44:03 +0700

    menambahkan berkas deployment

--- wishlist_page/urls.py
+++ wishlist_page/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from wishlist_page.views import search_books, add_to_wishlist, load_wishlist, delete_item_ajax, show_json, add_book_ajax
+from wishlist_page.views import search_books, add_to_wishlist, load_wishlist, delete_item_ajax, show_json, add_book_ajax, create_notes
 from landingPage.views import get_books_json  
 
 app_name = 'wishlist_page'
@@ -12,5 +12,5 @@ urlpatterns = [
     path('delete-item-ajax/<int:id>/', delete_item_ajax, name='delete_item_ajax'),
     path('json/', show_json, name='show_json'),
     path('add-book-ajax/', add_book_ajax, name='add_book_ajax'),
-
+    path('create-notes', create_notes, name='create_notes'),
 ]
