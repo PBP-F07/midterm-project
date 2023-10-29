197e4cbd9411da6beef811bee0f06fad79521635
zaimnazif974 <zaim.aydin@ui.ac.id>
2023-10-29 10:21:13 +0700

    comment done

--- book_details/models.py
+++ book_details/models.py
@@ -6,8 +6,10 @@ class discussion(models.Model):
     book = models.ForeignKey(Books, on_delete=models.CASCADE, default='')
     comment = models.CharField(max_length=500)
     user = models.CharField(max_length=255,default='')
+    date_added = models.CharField(max_length=255,default='')
 
 class reply(models.Model):
-    comment = models.ForeignKey(discussion, on_delete=models.DO_NOTHING, default='')
+    comment = models.ForeignKey(discussion, on_delete=models.CASCADE, default='')
     replies = models.CharField(max_length=255, default='')
     user = models.CharField(max_length=255,default='')
+    date_add = models.CharField(max_length=255,default='')
--- book_details/urls.py
+++ book_details/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import books_details,create_comment,get_comments,delete_comment
+from .views import books_details,create_comment,get_comments,delete_comment, get_replies, replies, create_reply,delete_reply
 
 app_name = 'book_details'
 
@@ -8,4 +8,9 @@ urlpatterns = [
     path('create-comment/<int:id>', create_comment, name="create_comment"),
     path('get-comments/<int:id>', get_comments, name="get_comments"),
     path('delete-comment/<int:book_id>/<int:id>', delete_comment, name='delete_comment'),
+    
+    path('comments/<int:id>',replies, name="replies" ),
+    path('comments/get-replies/<int:id>',get_replies, name="get_replies" ),
+    path('comments/create-reply/<int:id>', create_reply, name="create-reply"),
+    path('comments/delete-reply/<int:comment_id>/<int:id>', delete_reply, name='delete_reply')
 ]
